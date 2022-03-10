e35c362851c4cadcc43b89f7322e5a3266e93061
Sancta Marie <isancta@gmail.com>
2022-03-09 19:17:52 -0500

    practices project

---
+++ BankingAPPConsole_Sancta/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankingAPPConsole_Sancta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accounts User1 = new Accounts()
+            {
+                accNumber = 77,
+                accName = "Annick",
+                accType = "cheching",
+                accBalance = 1000,
+                accIsActive = True,
+                accEmail = "annick12@gmail.com"
+            }
+            
+        }
+    }
+}
