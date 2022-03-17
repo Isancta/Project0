9af373d6949bd91b67e4f2e9f3bd1f6d0268847b
Sancta Marie <isancta@gmail.com>
2022-03-16 22:25:58 -0400

    Banking app

--- BankingAPPConsole_Sancta/Accounts.cs
+++ BankingAPPConsole_Sancta/Accounts.cs
@@ -3,16 +3,19 @@ using System;
 class Accounts 
 {
 #region My Variables
-
+    static int autoAccNumber; 
     public int accNumber {get;set;}
     public string accName {get;set;}
     public string accType {get;set;}
     public double accBalance {get;set;}
     public bool accIsActive {get;set;}
     public string accEmail {get;set;} 
-
+    
 #endregion
-
+public Accounts(){
+autoAccNumber++;
+accNumber=autoAccNumber;
+}
 #region My Methods
 
 public double CheckBalance()
@@ -23,7 +26,7 @@ public double CheckBalance()
 public double Withdraw(double Amount)
 { accBalance -= Amount;
 return accBalance;
-//Console.WriteLine( " your accBalance is : "+accBalance);
+
 
 }
 public double deposit(double Amount)
@@ -36,17 +39,19 @@ public double deposit(double Amount)
 public void getAccountDetails()
 {
     Console.WriteLine( "Account No :"+accNumber);
-    //Console.WriteLine( "What's your Account No?");
-    //Console
     Console.WriteLine( "Name :"+ accName);
     Console.WriteLine( " Type :"+accType);
     Console.WriteLine( "Balance :"+accBalance);
     Console.WriteLine( "Email :"+accEmail);
     
 }
-#endregion
 
 
 
 
 }
+#endregion
+
+
+
+
--- BankingAPPConsole_Sancta/Program.cs
+++ BankingAPPConsole_Sancta/Program.cs
@@ -11,7 +11,7 @@ namespace BankingAPPConsole_Sancta
             {
                 accNumber = 77,
                 accName = "Annick",
-                accType = "cheching",
+                accType = "checking",
                 accBalance = 1000,
                 accIsActive = true,
                 accEmail = "annick12@gmail.com"
@@ -22,8 +22,75 @@ namespace BankingAPPConsole_Sancta
             Console.WriteLine(User.Withdraw(200)); 
             Console.WriteLine(User.deposit(2000));
             Console.WriteLine(User.CheckBalance());
-
+         bool Banking = true;
+ while (Banking ){
+            #region Menu
+            Console.ReadLine("");
+            Console.WriteLine(" Choose option below");
+            Console.WriteLine("");
+            Console.WriteLine("1. Create new account");
+            Console.WriteLine("2. Check Balance");
+            Console.WriteLine("3. Withdraw");
+            Console.WriteLine("4. Deposit");
+            Console.WriteLine("5. Get details");
+            Console.WriteLine("6. Exit");
             
+            
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            
+            switch(option)
+            {
+              case 1: 
+                      Console.WriteLine( "please enter your name");
+                      User.accName = Console.ReadLine();
+                      Console.WriteLine("Please enter the type of account you wish to open");
+                      User.accType= Console.ReadLine();
+                      Console.WriteLine("Please enter your account balance.");
+                      User.accBalance = Convert.ToDouble(Console.ReadLine());
+                      Console.WriteLine("Is the account active?");
+                      User.accIsActive = Convert.ToBoolean(Console.ReadLine());
+                      Console.WriteLine("Please enter your email");
+                      User.accEmail = Console.ReadLine();
+                      break;
+
+              case 2:  
+                      Console.WriteLine( " Your balance is :" + User.CheckBalance());
+                      break;
+
+              case 3:
+                      Console.WriteLine("How much would you like to withdraw?");
+                      double Withdraw_Amount= Convert.ToDouble(Console.ReadLine( ));
+                      Console.WriteLine(User.Withdraw( Withdraw_Amount));
+                      
+                      break;
+
+              case 4:
+                      Console.WriteLine("How much would you like to deposit?");
+                      double Deposit_Amount= Convert.ToDouble(Console.ReadLine( ));
+                      Console.WriteLine(User.deposit(Deposit_Amount));
+                      break;
+
+              case 5: 
+                      User.getAccountDetails();
+                      break;
+                     
+              case 6:
+                     Banking=false;
+                     Console.WriteLine("Exit");
+                     break;
+
+            }      
+
+
+
+            }
+           
+           
+           
+           
+           
+            #endregion
 
             
 
